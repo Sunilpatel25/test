3c70deab9e379490109591821926b8efd0de2cce
Sunilpatel25 <savaliyasunil3@gmail.com>
2025-09-12 08:43:19 +0530

    your message

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const songRoute = require("./routes/songRoute");
+const app = express();
+const PORT = 80;
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://savaliyasunil25_db_user:123@cluster1.96n9rvq.mongodb.net/songs?retryWrites=true&w=majority&appName=Cluster1",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use("/song", songRoute);
+
+app.listen(PORT, () => {
+  console.log("Server started at port http://localhost:" + PORT);
+});
